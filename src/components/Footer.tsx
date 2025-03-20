425b5df79862f59ae80d29135775a77d9f35bf7c
PlanC90 <167799741+PlanC90@users.noreply.github.com>
2025-03-20 16:31:02 +0300

    Initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,6 +18,15 @@ export const Footer: React.FC = () => {
             your financial advisor before making any investment decision.
           </p>
         </div>
+        <div className="text-center mb-4">
+          <h6>Follow us on social media</h6>
+          <a href="https://x.com/memexairdrop" className="mr-2">
+            🐦
+          </a>
+          <a href="https://t.me/MemeXGloball">
+            ✈️
+          </a>
+        </div>
         <div className="text-center text-gray-400">
           &copy; {new Date().getFullYear()} MemeX. All rights reserved.
         </div>
@@ -29,15 +38,6 @@ export const Footer: React.FC = () => {
           This site belongs to the MemeX Community. The fee for each NFT sold
           will be burned. NFTs sold via XEP will buy MEMEX from the market and
           burn it, while those sold directly with MEMEX will be burned directly.
-        </div>
-        <div className="text-center mt-4">
-          <h6>Follow us on social media</h6>
-          <a href="https://x.com/memexairdrop" className="mr-2">
-            🐦
-          </a>
-          <a href="https://t.me/MemeXGloball">
-            ✈️
-          </a>
         </div>
       </div>
     </footer>
