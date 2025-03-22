023eca59ec81c8f252106fcd590216337d11bc9c
PlanC90 <167799741+PlanC90@users.noreply.github.com>
2025-03-22 18:59:49 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,39 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+    import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['lucide-react', 'uuid'],
-  },
-  server: {
-    proxy: {
-      '/data': 'http://localhost:3000'
-    },
-    hmr: {
-      overlay: false,
-      protocol: 'ws',
-    },
-    allowedHosts: ["nft.memextoken.org"]
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+    export default defineConfig({
+      plugins: [react()],
+      optimizeDeps: {
+        include: ['lucide-react', 'uuid'],
+      },
+      server: {
+        proxy: {
+          '/data': {
+            target: 'http://localhost:3000',
+            secure: false, // set to true if your target is HTTPS
+          },
+          '/socket.io': { // If you are using socket.io
+            target: 'ws://localhost:3000',
+            ws: true,
+            secure: false, // set to true if your target is WSS
+          }
+        },
+        hmr: {
+          overlay: false,
+          protocol: 'ws',
+        },
+        allowedHosts: ["nft.memextoken.org"]
+      },
+      build: {
+        outDir: 'dist',
+        sourcemap: false,
+        rollupOptions: {
+          output: {
+            manualChunks: {
+              vendor: ['react', 'react-dom', 'react-router-dom'],
+            },
+          },
         },
       },
-    },
-  },
-  base: '/',
-});
+      base: '/',
+    });
