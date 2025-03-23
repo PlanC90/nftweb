1d78c5370147f480cae9d57873924c1ef6114439
PlanC90 <167799741+PlanC90@users.noreply.github.com>
2025-03-23 15:45:36 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,38 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+    import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['lucide-react', 'uuid'],
-  },
-  server: {
-    proxy: {
-      '/data': {
-        target: 'https://nft.memextoken.org',
-        secure: true,
-        changeOrigin: true,
+    export default defineConfig({
+      plugins: [react()],
+      optimizeDeps: {
+        include: ['lucide-react', 'uuid'],
       },
-      '/socket.io': {
-        target: 'wss://nft.memextoken.org',
-        ws: true,
-        secure: true,
-        changeOrigin: true,
-      }
-    },
-    hmr: {
-      overlay: false,
-    },
-    allowedHosts: ["nft.memextoken.org"]
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+      server: {
+        proxy: {
+          '/data': {
+            target: 'https://localhost:3000',
+            secure: true,
+          },
+          '/socket.io': {
+            target: 'wss://localhost:3000',
+            ws: true,
+            secure: true,
+          }
+        },
+        hmr: {
+          overlay: false,
+        },
+        allowedHosts: ["nft.memextoken.org"]
+      },
+      build: {
+        outDir: 'dist',
+        sourcemap: false,
+        rollupOptions: {
+          output: {
+            manualChunks: {
+              vendor: ['react', 'react-dom', 'react-router-dom'],
+            },
+          },
         },
       },
-    },
-  },
-  base: '/',
-  define: {
-    'global': 'window',
-  },
-});
+      base: '/',
+    });
